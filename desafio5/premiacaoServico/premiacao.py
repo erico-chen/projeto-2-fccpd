1f4055193841ec71ac48a4487793cbe7fae28162
Erico <erico.chen@hotmail.com>
2025-11-20 09:39:09 -0300

    [Refactor]: alteta o json

--- desafio5/premiacaoServico/premiacao.py
+++ desafio5/premiacaoServico/premiacao.py
@@ -3,9 +3,9 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 PREMIACAO = [
-    {"id": 101, "usuario_id": 1, "premiacao_carreira": 50000000},
-    {"id": 102, "usuario_id": 2, "premiacao_carreira": 49300000},
-    {"id": 103, "usuario_id": 3, "premiacao_carreira": 24300000},
+    {"id": 101, "tenista_id": 1, "premiacao_carreira": 50000000},
+    {"id": 102, "tenista_id": 2, "premiacao_carreira": 49300000},
+    {"id": 103, "tenista_id": 3, "premiacao_carreira": 24300000},
 ]
 
 
